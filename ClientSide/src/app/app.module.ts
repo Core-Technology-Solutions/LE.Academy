79706a862843b419fcad375b40c38c22a8ffde0c
Lamiaa Selim <lamiaa.selim@cts-egy.com>
2023-08-31 16:37:54 +0300

    header

--- ClientSide/src/app/app.module.ts
+++ ClientSide/src/app/app.module.ts
@@ -6,10 +6,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './Components/nav/nav.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SliderComponent } from './Components/slider/slider.component';
+import { BoxesComponent } from './Components/boxes/boxes.component';
+import { AboutComponent } from './Components/about/about.component';
+import { CoursesComponent } from './Components/courses/courses.component';
+import { ImagesComponent } from './Components/images/images.component';
+import { ImportantComponent } from './Components/important/important.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    SliderComponent,
+    BoxesComponent,
+    AboutComponent,
+    CoursesComponent,
+    ImagesComponent,
+    ImportantComponent,
   ],
   imports: [
     BrowserModule,
