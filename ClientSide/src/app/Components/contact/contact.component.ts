d1f568485d44eeebd044c61c76924d851775eb0c
Lamiaa Selim <lamiaa.selim@cts-egy.com>
2023-09-19 13:55:29 +0300

    edit-routing

--- ClientSide/src/app/Components/contact/contact.component.ts
+++ ClientSide/src/app/Components/contact/contact.component.ts
@@ -14,23 +14,31 @@ export class ContactComponent {
   phone: string = '';
   selectedCourse: string = '';
   isSuccess: boolean = false; // Track the success state
+  isSubmitDisabled: boolean = false;
 
   constructor(private emailService: EmailServiceService) {}
 
-
   onSubmit() {
+    // Disable the submit button
+    this.isSubmitDisabled = true;
+
     this.emailService.sendEmail(this.name, this.email, this.phone, this.selectedCourse).subscribe(
       response => {
-        if (response === 'Email sent successfully') { // Check for the exact text response
+        if (response === 'Email sent successfully') {
           this.isSuccess = true;
         } else {
           console.log('Unexpected response:', response);
         }
+
+        // Re-enable the submit button regardless of the response
+        this.isSubmitDisabled = false;
       },
       error => {
         console.log('Error sending email:', error);
+
+        // Re-enable the submit button in case of an error
+        this.isSubmitDisabled = false;
       }
     );
   }
-
 }
